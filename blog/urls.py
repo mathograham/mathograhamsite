f460189a26ce34516071cf1c11f2a573c87a8842
Your Name <you@example.com>
2021-07-14 14:16:52 -0400

    Added view and template for detailed blog post and CSS for site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+
+#Tells django what to do with appendage to site's url
+#first string in path is the appendage,
+#second element is the view in views.py(must be created)
+#third element is the name. Name used in html when calling django code
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
